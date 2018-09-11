f3b014bb024d3d83363224d9a0da8d9900ab5e20
matthew <matthewgan@126.com>
2018-09-11 19:29:43 +0800

    update settings to collect static

--- ModelServer/settings.py
+++ ModelServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o!16=ih&v%jgep+!=cq3!m0#c&t2mry+kf#rxwqa(1+n42nj3v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "23.100.122.129"]
 
 
 # Application definition
